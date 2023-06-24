fd9353cc21067fe08b07b38e7b7fb72c2133d4b9
namiphuh10 <namihuh10@gmail.com>
2023-06-25 02:24:32 +0900

    구조 변경

--- src/main/java/com/nsplit/controller/MeetingController.java
+++ src/main/java/com/nsplit/controller/MeetingController.java
@@ -1,4 +1,19 @@
 package com.nsplit.controller;
 
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
 public class MeetingController {
+
+
+    @RequestMapping("/insertfee.do")
+    @ResponseBody
+    public void insertPay() {
+        System.out.println("===========================");
+    }
+
 }
